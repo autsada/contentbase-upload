0006449953dc75c057552eb268622593d0d9939b
autsada <autsada.t@gmail.com>
2023-02-28 15:31:18 +0700

    use google client library

--- src/middlewares/verify-token.ts
+++ src/middlewares/verify-token.ts
@@ -1,8 +1,12 @@
 import type { Request, Response, NextFunction } from "express"
 import axios from "axios"
 
-const { KMS_ACCESS_KEY, KMS_BASE_URL } = process.env
+import { Environment } from "../types"
+import { authClient } from "../utils/authClient"
 
+const { KMS_ACCESS_KEY, KMS_BASE_URL, NODE_ENV } = process.env
+
+const env = NODE_ENV as Environment
 export async function verifyIdToken(
   req: Request,
   res: Response,
@@ -12,11 +16,14 @@ export async function verifyIdToken(
     const authorization = req.headers["authorization"]
     const idToken = authorization?.split(" ")[1]
 
+    // The token for use to authenticate between services in GCP
+    const token = env !== "development" ? await authClient.getIdToken() : ""
     // Call the `KMS` service to verify id token
     const result = await axios({
       method: "GET",
       url: `${KMS_BASE_URL}/token/verify`,
       headers: {
+        Authorization: token,
         "id-token": idToken || "",
         "x-access-key": KMS_ACCESS_KEY || "",
       },
